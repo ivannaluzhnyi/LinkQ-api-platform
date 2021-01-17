61926004731efb4f9db2095f06d0a121d6452ee3
Bryan Gatay <gatay.bryan@gmail.com>
2021-01-18 00:25:36 +0100

    Update fixtures Properties

--- src/DataFixtures/PropertyFixtures.php
+++ src/DataFixtures/PropertyFixtures.php
@@ -40,25 +40,29 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create();
         for ($i = 0; $i < 60; ++$i) {
-            $property = new Property();
-            $property->setTitle($faker->sentence(5));
-            $property->setDescription($faker->paragraph);
-            $property->setPrice($faker->numberBetween(50000, 1500000));
-            $property->setAddress(
-                (new Address())
-                    ->setStreet($faker->streetAddress)
-                    ->setZipcode($faker->postcode)
-                    ->setCity($faker->city)
-                    ->setCountry($faker->country)
-            );
-            $property->setFeatures(
-                (new Feature())
+
+            $feature = (new Feature())
                     ->setSize($faker->numberBetween(9, 250))
                     ->setRooms($faker->numberBetween(1, 10))
                     ->setBathrooms($faker->numberBetween(1, 4))
                     ->setBedrooms($faker->numberBetween(1, 5))
-                    ->setGarages($faker->numberBetween(1, 3))
-            );
+                    ->setGarages($faker->numberBetween(1, 3));
+
+            
+            $address = (new Address())
+                ->setStreet($faker->streetAddress)
+                ->setZipcode($faker->postcode)
+                ->setCity($faker->city)
+                ->setCountry($faker->country);
+                
+            $manager->persist($address);
+            $manager->persist($feature);
+            $property = new Property();
+            $property->setTitle($faker->sentence(5));
+            $property->setDescription($faker->paragraph);
+            $property->setPrice($faker->numberBetween(50000, 1500000));
+            $property->setAddress($address);
+            $property->setFeatures($feature);
             $property->setUserRelated(null);
 
             $nbOfMedias = rand(1, 5);
@@ -70,6 +74,10 @@ class PropertyFixtures extends Fixture implements DependentFixtureInterface
             $this->mediaCursor += $nbOfMedias;
 
             $manager->persist($property);
+            $feature->setProperty($property);
+            $address->setProperty($property);
+            $manager->persist($feature);
+            $manager->persist($address);
         }
         $manager->flush();
     }
